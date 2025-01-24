f2ada50b299ccc90a7e7ad549d4e15ced338e699
Snake-Pl1ssken <jorgeluisgarciorbegoso@gmail.com>
2025-01-24 11:01:16 +0100

    Test de guardar partida

---
+++ Assets/save/Scripts/SaveGameStrings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveGameStrings
+{
+    public static string posicionX = "PosicionX";
+    public static string posicionY = "PosicionY";
+    public static string posicionZ = "PosicionZ";
+}
