fcc37b609ff39df0cf9b08a25287d97d5cdbd6ac
Jorge Luis Garcia Orbegoso <jorgeluisgarciaorbegoso@gmail.com>
2025-01-26 19:47:59 +0100

    Añadido movimiento basico con salto del personaje asi como tambie un mapeado basico de su habitat

---
+++ Assets/Player/Scripts_Test/PlayerLocomotionInput.cs
@@ -0,0 +1,19 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//namespace GinjaGaming.FinalCharacterController
+//{ 
+//    public class PlayerLocomotionInput : MonoBehaviour, PlayerControls.IPlayerLocomotionMapActions
+//    {
+//        public PlayerControls PlayerControls { get; private set; }
+//        public Vector2 MovementInput { get; private set; }
+
+//        public void OnMovement(InputAction.CallnackContext context)
+//        { 
+        
+//        }
+//    }
+
+//}
+
