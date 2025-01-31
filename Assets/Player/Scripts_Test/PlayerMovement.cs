0b12d652fc0feebe8973292e31cf19dfeb778d2d
javiermoliina03 <javiermoliina03@gmail.com>
2025-01-31 10:59:27 +0100

    Camara 3a persona empezada e input actions metidas

--- Assets/Player/Scripts_Test/PlayerMovement.cs
+++ Assets/Player/Scripts_Test/PlayerMovement.cs
@@ -1,10 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] InputActionReference Salto;
+    [SerializeField] InputActionReference Caminar;
+    [SerializeField] InputActionReference Correr;
+    [SerializeField] InputActionReference Derecha;
+    [SerializeField] InputActionReference Izquierda;
+    [SerializeField] InputActionReference Atras;
+    [SerializeField] InputActionReference GamePad;
     [SerializeField] float speed = 5f;
     [SerializeField] float jumpForce = 5f;
     [SerializeField] float gravity = -9.8f;
@@ -12,40 +20,76 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float verticalVelocity;
 
     CharacterController characterController;
+    //CONTROL PLAYERCAM
+
+    //CONTROL PLAYERCAM
 
     void Awake()
     {
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
         characterController = GetComponent<CharacterController>();    
+    }
+    private void OnEnable()
+    {
+        Caminar.action.Enable();
+        Correr.action.Enable();
+        Salto.action.Enable();
+        Derecha.action.Enable();
+        Izquierda.action.Enable();
+        Atras.action.Enable();
+        GamePad.action.Enable();
+    }
+    private void OnDisable()
+    {
+        Caminar.action.Disable();
+        Correr.action.Disable();
+        Salto.action.Disable();
+        Derecha.action.Disable();
+        Izquierda.action.Disable();
+        Atras.action.Disable();
+        GamePad.action.Disable();
+    }
+    public void OnMove()
+    { 
+    
     }
     void Update()
     {
         Vector3 movementInput = Vector3.zero;
-        
-        //Movement onGround Player
-        if (Input.GetKey(KeyCode.W))
+        Vector2 joystickInput = Vector2.zero;
+        //Movement onGround Player KEYBOARD
+
+        if (Caminar.action.IsPressed())
         {
             movementInput.z = 1;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Atras.action.IsPressed())
         {
             movementInput.z = -1;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Derecha.action.IsPressed())
         {
             movementInput.x = 1;
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Izquierda.action.IsPressed())
         {
             movementInput.x = -1;
         }
+        //Movement onGround Player GAMEPAD
+        if (GamePad.action.WasPressedThisFrame())
+        {
+            joystickInput = GamePad.action.ReadValue<Vector2>();
+        }
+        Vector3 move = new Vector3(joystickInput.x, 0, joystickInput.y) * speed * Time.deltaTime;
         //Movement onGround Player
 
 
         //Movement !onGround Player (Salto)
         if (characterController.isGrounded)
         {
-            if (Input.GetKey(KeyCode.Space))
+            if (Salto.action.WasPressedThisFrame())
             {
                 verticalVelocity = jumpForce;
             }
