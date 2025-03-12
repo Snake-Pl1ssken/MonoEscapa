5d3bb63b8689e167f8c4a45d64fac4778cd350f3
javiermoliina03 <javiermoliina03@gmail.com>
2025-03-12 14:26:56 +0100

    enemigos ahora patrullan y drop de monedas

--- Assets/Enemy/Scripts/EnemyAI.cs
+++ Assets/Enemy/Scripts/EnemyAI.cs
@@ -5,15 +5,18 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    public static EnemyAI Instance { get; private set; }
+
     public NavMeshAgent agent;
 
     public Transform player;
-
+    [SerializeField]public Transform[] patrolpuntos;
     public LayerMask whatisGround, whatisPlayer;
-
+    int targetPoint = 0;
     //Patrol
     public Vector3 walkPoint;
     bool walkPointSet;
+    bool viendoPlayer = false;
     public float walkPointRange;
     //Chase
     public float sightRange, attackRange;
@@ -23,6 +26,7 @@ public class EnemyAI : MonoBehaviour
     bool alreadyAttacked;
     public GameObject projectile;
     public int health = 3;
+
     private void Awake()
     {
         player = GameObject.Find("Player_Object").transform;
@@ -31,25 +35,48 @@ public class EnemyAI : MonoBehaviour
 
     private void Patroling()
     {
-        if (!walkPointSet) SearchWalkPoint();
+        viendoPlayer = false;
+        if (viendoPlayer == false)
+        {
+            agent.SetDestination(patrolpuntos[targetPoint].transform.position);
+            if (transform.position.x == patrolpuntos[targetPoint].transform.position.x && transform.position.z == patrolpuntos[targetPoint].transform.position.z)
+            {
+                SearchWalkPoint();
+            }
+        }
     }
     private void SearchWalkPoint()
     {
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatisGround))
-        { 
-            walkPointSet = true;
-        }
+       // float randomZ = Random.Range(-walkPointRange, walkPointRange);
+       // float randomX = Random.Range(-walkPointRange, walkPointRange);
+       // walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+       // if (Physics.Raycast(walkPoint, -transform.up, 2f, whatisGround))
+       // { 
+       //     walkPointSet = true;
+       // }
+        
+            if (transform.position.x == patrolpuntos[targetPoint].transform.position.x && transform.position.z == patrolpuntos[targetPoint].transform.position.z)
+            { 
+                targetPoint++; 
+                if (targetPoint == patrolpuntos.Length)
+                {
+                targetPoint = 0;
+                }
+            }
+            
+            
+        
+        
     }
     private void Chaseplayer()
     {
+        viendoPlayer = true;
         agent.SetDestination(player.position);
+        
     }
     private void Attackplayer()
     {
+        viendoPlayer = true;
         agent.SetDestination(transform.position);
         transform.LookAt(player);
         if (!alreadyAttacked) 
@@ -87,7 +114,7 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SearchWalkPoint();
     }
 
     // Update is called once per frame
